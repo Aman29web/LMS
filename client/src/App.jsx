d6da1a90ceb7eb3ab8cbab247552cdecd2a80704
Aman29web <amansinghvns29@gmail.com>
2025-02-25 12:47:00 +0530

    Home page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useMatch } from 'react-router-dom'
 import Home from './pages/student/Home'
 import CoursesList from './pages/student/CoursesList'
 import MyEnrollments from './pages/student/MyEnrollments'
@@ -11,6 +11,7 @@ import Dashboard from './pages/educator/Dashboard'
 import AddCourse from './pages/educator/AddCourses'
 import MyCourses from './pages/educator/MyCourses'
 import StudentsEnrolled from './pages/educator/StudentEnrolled'
+import Navbar from './components/student/Navbar'
 // import AddCourse from './pages/educator/AddCourse'
 
 
@@ -18,8 +19,14 @@ import StudentsEnrolled from './pages/educator/StudentEnrolled'
 
 
 const App = () => {
+
+  const isEducatorRoute = useMatch('/educator/*')
+ 
+
   return (
-    <div>
+    <div className='text-default min-h-screen bg-white'>
+     
+      {!isEducatorRoute &&   <Navbar/>}
       <Routes>
         <Route path ='/' element={<Home />} />
         <Route path ='/course-list' element={<CoursesList />} />
--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
+import CourseCard from './CourseCard'
 
 const CoursesSection = () => {
+
+  const {allCourses} =useContext(AppContext)
   return (
-    <div>
+    <div className='py-16 md:px-40 px-8'>
+      <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
+      <p className='text-sm md:text-base text-gray-500 mt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore,<br></br> distinctio. Sapiente, saepe amet cumque nulla iste vero a.</p>
       
+      <div className='grid grid-cols-auto px-4 md:px-0 md:my-16 my-10 gap-4'>
+        {allCourses.slice(0,4).map((course,index)=><CourseCard key={index} course={course}/>)}
+      </div>
+
+      <Link to={'</div>/course-list'} onClick={()=> scrollTo(0,0)} className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'>Show all courses</Link>
     </div>
   )
 }
